930406538e701bb4a35a027103042b8951c27e73
Mex777 <fologeaalexandru@gmail.com>
2022-04-22 22:08:05 +0300

    made the reset button and the number of pixels functionality

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-for (let i = 0; i < 16; ++i) {
-  const currRow = document.createElement('div');
-  currRow.className = 'row';
+const maxWidth = 512;
+const maxHeight = 512;
+const grid = document.getElementById('grid');
 
-  for (let j = 0; j < 16; ++j) {
-    const currDiv = document.createElement('div');
-    currDiv.id = i + '-' + j;
-    currDiv.className = 'box';
-    currDiv.addEventListener('mouseover', () => {
-      currDiv.style.backgroundColor = 'red';
-    });
-    currRow.appendChild(currDiv);
+const makeGrid = (size) => {
+  for (let i = 0; i < size; ++i) {
+    const currRow = document.createElement('div');
+    currRow.className = 'row';
+
+    for (let j = 0; j < size; ++j) {
+      const currDiv = document.createElement('div');
+      currDiv.id = i + '-' + j;
+      currDiv.className = 'box';
+      currDiv.style.width = maxWidth / size;
+      currDiv.style.height = maxHeight / size;
+      currDiv.addEventListener('mouseover', () => {
+        currDiv.style.backgroundColor = 'red';
+      });
+      currRow.appendChild(currDiv);
+    }
+
+    grid.appendChild(currRow);
   }
+};
 
-  document.getElementById('grid').appendChild(currRow);
-}
+makeGrid(50);
+
+const reset = () => {
+  const rows = grid.childNodes;
+  Array.from(rows).forEach((node) => grid.removeChild(node));
+  let size = prompt('Size', 'A value between 1 and 100');
+  while (size < 1 || size > 100) {
+    size = prompt('Size', 'A value between 1 and 100');
+  }
+  makeGrid(size);
+};
